484cb6057fcfee2472bf4db94b16e8023618efe4
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2024-07-13 20:33:48 -0300

    implantando o cors no pprojeto

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,20 +1,24 @@
-// src/app.js
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const horarioRoutes = require('./routes/horarioRoutes');
-const produtoRoutes = require('./routes/produtoRoutes'); 
-const safraRoutes = require('./routes/safraRoutes'); 
+const produtoRoutes = require('./routes/produtoRoutes');
+const safraRoutes = require('./routes/safraRoutes');
 const transportadoraRoutes = require('./routes/transportadoraRoutes');
-const veiculoRoutes = require('./routes/veiculoRoutes'); 
-const portariaRoutes = require('./routes/portariaRoutes'); 
-const agendamentoRoutes = require('./routes/agendamentoRoutes'); 
-const authRoutes = require('./routes/authRoutes');
+const veiculoRoutes = require('./routes/veiculoRoutes');
+const portariaRoutes = require('./routes/portariaRoutes');
+const agendamentoRoutes = require('./routes/agendamentoRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
+// Configure o CORS
+app.use(cors({
+    origin: 'https://logisync-frontend.azurewebsites.net', 
+    optionsSuccessStatus: 200,
+}));
 
 app.use(express.json());
-app.use('/api', authRoutes);
 
 app.use('/api', userRoutes);
 app.use('/api', horarioRoutes);
@@ -24,8 +28,9 @@ app.use('/api', transportadoraRoutes);
 app.use('/api', veiculoRoutes);
 app.use('/api', portariaRoutes);
 app.use('/api', agendamentoRoutes);
-app.use('/api', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+module.exports = db;
