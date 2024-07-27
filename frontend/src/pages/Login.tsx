baccc173b5e919607f0206b56fccf3c8b77eae7c
Blackburn060 <Blacktauz30@outlook.com>
2024-07-27 17:18:37 -0300

    Correção de commit

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,16 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import logo from '../assets/images/Logo-LogiSync-Horizontal-02-SF.png';
+import logoHorizontal from '../assets/images/Logo-LogiSync-Horizontal-02-SF.png';
+import imagemLateralLogin from '../assets/images/Imagem-Lateral-Login.png';
 
 const backendUrl = import.meta.env.VITE_APP_BACKEND_API_URL;
 
-<<<<<<< HEAD
 console.log(import.meta.env);
 console.log(backendUrl);
 
-=======
->>>>>>> parent of d2a36d6 (Novas alterações)
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -39,10 +37,10 @@ const Login: React.FC = () => {
   return (
     <div className="flex h-screen">
       <div className="w-1/2 flex items-center justify-center bg-gray-100">
-        <img src={logo} alt="LogiSync Logo" className="w-72 h-52" />
+        <img src={imagemLateralLogin} alt="LogiSync Logo" className="w-auto h-auto" />
       </div>
       <div className="w-1/2 flex flex-col justify-center items-center bg-logisync-color-blue-400">
-        <img src={logo} alt="LogiSync Logo" className="w-72 h-52" />
+        <img src={logoHorizontal} alt="LogiSync Logo" className="w-64 h-44" />
         <form onSubmit={handleSubmit} className="w-full max-w-sm">
           <div className="mb-4">
             <label htmlFor="email" className="block text-white text-sm font-bold mb-2">Email</label>
@@ -54,6 +52,7 @@ const Login: React.FC = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
+              autoComplete="username"
             />
           </div>
           <div className="mb-6">
