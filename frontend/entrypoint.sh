b79913de64afa9858856b165b8fe69b649557949
Blackburn060 <Blacktauz30@outlook.com>
2024-07-27 17:19:18 -0300

    Revert "Novas correções das variáveis de ambiente"
    This reverts commit fd715682e8c73083519aec338229e076f258993b.

--- frontend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Atribuir valores às variáveis de ambiente do frontend
-echo "VITE_APP_BACKEND_API_URL=${VITE_APP_BACKEND_API_URL}" >> /usr/share/nginx/html/env-config.js
-
-# Iniciar o Nginx
-exec "$@"
