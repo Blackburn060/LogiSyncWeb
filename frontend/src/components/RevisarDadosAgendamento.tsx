60db6c402b44146749698a4760b745147dc9e47f
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2024-08-29 00:26:20 -0300

    Ajuste Modal - Dados Agendamento

--- frontend/src/components/RevisarDadosAgendamento.tsx
+++ frontend/src/components/RevisarDadosAgendamento.tsx
@@ -4,13 +4,13 @@ import { Horario } from '../models/Horario';
 import { getUsuario } from '../services/usuarioService';
 import { getVeiculos } from '../services/veiculoService';
 import { getTransportadora } from '../services/transportadoraService';
-import { getProdutos } from '../services/produtoService';
 import { addAgendamento } from '../services/agendamentoService';
+import { getProdutos } from '../services/produtoService';  // Importando o serviço de produtos
 import { useAuth } from '../context/AuthContext';
 import { Usuario } from '../models/Usuario';
 import { Veiculo } from '../models/Veiculo';
-import { Produto } from '../models/Produto';
 import { Transportadora } from '../models/Transportadora';
+import { Produto } from '../models/Produto';  // Importando o modelo de produtos
 
 interface RevisarDadosAgendamentoProps {
   selectedDate: Date;
@@ -18,18 +18,12 @@ interface RevisarDadosAgendamentoProps {
   onClose: () => void;
 }
 
-const RevisarDadosAgendamento: React.FC<RevisarDadosAgendamentoProps> = ({
-  selectedDate,
-  horarioSelecionado,
-  onClose,
-}) => {
+const RevisarDadosAgendamento: React.FC<RevisarDadosAgendamentoProps> = ({ selectedDate, horarioSelecionado, onClose }) => {
   const { accessToken, user } = useAuth();
   const [usuario, setUsuario] = useState<Usuario | null>(null);
   const [veiculos, setVeiculos] = useState<Veiculo[]>([]);
-  const [transportadora, setTransportadora] = useState<Transportadora | null>(
-    null
-  );
-  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [transportadora, setTransportadora] = useState<Transportadora | null>(null);
+  const [produtos, setProdutos] = useState<Produto[]>([]);  // Estado para armazenar os produtos
   const [veiculoSelecionado, setVeiculoSelecionado] = useState<string>('');
   const [produto, setProduto] = useState<string>('');
   const [quantidade, setQuantidade] = useState<string>('');
@@ -46,22 +40,17 @@ const RevisarDadosAgendamento: React.FC<RevisarDadosAgendamentoProps> = ({
 
           const veiculosData = await getVeiculos(accessToken);
           console.log('Veículos disponíveis:', veiculosData);
-          setVeiculos(
-            veiculosData.filter((veiculo) => veiculo.SituacaoVeiculo === 1)
-          );
-
-          if (usuarioData.CodigoTransportadora) {
-            const transportadoraData = await getTransportadora(
-              accessToken,
-              usuarioData.CodigoTransportadora
-            );
-            console.log('Dados da transportadora:', transportadoraData);
-            setTransportadora(transportadoraData);
-          }
+          setVeiculos(veiculosData.filter(veiculo => veiculo.SituacaoVeiculo === 1));
 
           const produtosData = await getProdutos(accessToken);
           console.log('Produtos disponíveis:', produtosData);
-          setProdutos(produtosData);
+          setProdutos(produtosData);  // Carregando os produtos no estado
+
+          if (usuarioData.CodigoTransportadora) {
+            const transportadoraData = await getTransportadora(accessToken, usuarioData.CodigoTransportadora);
+            console.log('Dados da transportadora:', transportadoraData);
+            setTransportadora(transportadoraData);
+          }
         } catch (error) {
           console.error('Erro ao buscar dados:', error);
         }
@@ -107,128 +96,115 @@ const RevisarDadosAgendamento: React.FC<RevisarDadosAgendamentoProps> = ({
 
   return (
     <Modal
-      isOpen={true}
-      onRequestClose={onClose}
-      contentLabel="Revisar Agendamento"
-      className="bg-white p-6 rounded-lg shadow-lg max-w-4xl w-full overflow-y-auto -mt-"
-      overlayClassName="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-start pt-16"
-    >
-      <div className="flex flex-col space-y-4 h-full">
-        <h2 className="text-xl font-bold text-center">Revisar Agendamento</h2>
-
-        <div className="border p-4 rounded-lg">
-          <h3 className="text-lg font-semibold mb-2">Dados Pessoais</h3>
-          <div>
-            <strong>Nome:</strong> {usuario?.NomeCompleto || 'Não disponível'}
-          </div>
-          <div>
-            <strong>Telefone:</strong> {usuario?.NumeroCelular || 'Não disponível'}
-          </div>-mt-10
-          <div>
-            <strong>CPF:</strong> {usuario?.CPF || 'Não disponível'}
-          </div>
-        </div>
-
-        <div className="border p-4 rounded-lg">
-          <h3 className="text-lg font-semibold mb-2">Transportadora</h3>
-          <div>
-            <strong>Nome fantasia:</strong>{' '}
-            {transportadora?.NomeFantasia || 'Não disponível'}
-          </div>
-          <div>
-            <strong>Empresa:</strong>{' '}
-            {transportadora?.Nome || 'Transportadora não registrada'}
-          </div>
-          <div>
-            <strong>CPF/CNPJ:</strong> {transportadora?.CNPJ || 'Não disponível'}
-          </div>
-        </div>
-
-        <div className="border p-4 rounded-lg">
-          <h3 className="text-lg font-semibold mb-2">Veículo</h3>
-          <select
-            className="border rounded p-2 w-full"
-            value={veiculoSelecionado}
-            onChange={(e) => setVeiculoSelecionado(e.target.value)}
-          >
-            <option value="">Selecione</option>
-            {veiculos.map((veiculo) => (
-              <option key={veiculo.CodigoVeiculo} value={veiculo.CodigoVeiculo}>
-                {veiculo.NomeVeiculo}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="border p-4 rounded-lg">
-          <h3 className="text-lg font-semibold mb-2">Dados do Agendamento</h3>
-          <div>
-            <strong>Data:</strong> {selectedDate.toLocaleDateString()}
-          </div>
-          <div>
-            <strong>Horário:</strong> {horarioSelecionado.horarioInicio} -{' '}
-            {horarioSelecionado.horarioFim}
-          </div>
-          <div>
-            <label>
-              <strong>Produto (Opcional):</strong>
-            </label>
-            <select
-              className="border rounded p-2 w-full"
-              value={produto}
-              onChange={(e) => setProduto(e.target.value)}
-            >
-              <option value="">Selecione</option>
-              {produtos.map((produto) => (
-                <option key={produto.CodigoProduto} value={produto.CodigoProduto}>
-                  {produto.DescricaoProduto}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label>
-              <strong>Quantidade (Opcional):</strong>
-            </label>
-            <input
-              type="text"
-              className="border rounded p-2 w-full"
-              placeholder="Quantidade"
-              value={quantidade}
-              onChange={(e) => setQuantidade(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>
-              <strong>Observação:</strong>
-            </label>
-            <textarea
-              className="border rounded p-2 w-full"
-              placeholder="Insira uma observação"
-              value={observacao}
-              onChange={(e) => setObservacao(e.target.value)}
-            ></textarea>
-          </div>
-          <div>
-            <label>
-              <strong>Upload de Arquivo:</strong>
-            </label>
-            <input
-              type="file"
-              className="border rounded p-2 w-full"
-              onChange={(e) => setArquivo(e.target.files?.[0] || null)}
-            />
-          </div>
-        </div>
-
-        <button
-          onClick={handleAgendar}
-          className="bg-blue-500 text-white py-2 px-4 rounded mt-4"
-        >
-          Confirmar Agendamento
-        </button>
+  isOpen={true}
+  onRequestClose={onClose}
+  contentLabel="Revisar Agendamento"
+  className="bg-white p-2 rounded-lg shadow-lg max-w-2xl w-full max-h-[calc(95vh-01px)] overflow-y-auto z-[1050] mt-3"
+  overlayClassName="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-start z-[1040]"
+>
+  <div className="flex flex-col h-full">
+    {/* Cabeçalho Fixo */}
+    <div className="sticky top-0 bg-white z-[1060] p-4 border-b border-gray-200 flex justify-between items-center">
+      <h2 className="text-xl font-bold">Revisar Agendamento</h2>
+      <button
+        onClick={onClose}
+        className="bg-red-600 text-white font-bold border border-red-600 rounded px-2 py-1 hover:bg-red-700 transition"
+      >
+        Fechar
+      </button>
+    </div>
+    
+    {/* Conteúdo Rolável */}
+    <div className="flex flex-col space-y-2 p-4">
+      {/* Dados Pessoais */}
+      <div className="border p-3 rounded-lg">
+        <h3 className="text-lg font-semibold mb-1">Dados Pessoais</h3>
+        <div><strong>Nome:</strong> {usuario?.NomeCompleto || 'Não disponível'}</div>
+        <div><strong>Telefone:</strong> {usuario?.NumeroCelular || 'Não disponível'}</div>
+        <div><strong>CPF:</strong> {usuario?.CPF || 'Não disponível'}</div>
       </div>
-    </Modal>
+
+      {/* Transportadora */}
+      <div className="border p-3 rounded-lg">
+        <h3 className="text-lg font-semibold mb-1">Transportadora</h3>
+        <div><strong>Nome fantasia:</strong> {transportadora?.NomeFantasia || 'Não disponível'}</div>
+        <div><strong>Empresa:</strong> {transportadora?.Nome || 'Transportadora não registrada'}</div>
+        <div><strong>CPF/CNPJ:</strong> {transportadora?.CNPJ || 'Não disponível'}</div>
+      </div>
+
+      {/* Veículo */}
+      <div className="border p-3 rounded-lg">
+        <h3 className="text-lg font-semibold mb-1">Veículo</h3>
+        <select
+          className="border rounded p-2 w-full"
+          value={veiculoSelecionado}
+          onChange={(e) => setVeiculoSelecionado(e.target.value)}
+        >
+          <option value="">Selecione</option>
+          {veiculos.map((veiculo) => (
+            <option key={veiculo.CodigoVeiculo} value={veiculo.CodigoVeiculo}>
+              {veiculo.NomeVeiculo}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Dados do Agendamento */}
+      <div className="border p-3 rounded-lg">
+  <h3 className="text-lg font-semibold mb-1">Dados do Agendamento</h3>
+  <div className="flex space-x-2 mb-4"> {/* Adiciona uma margem inferior */}
+    <div><strong>Data:</strong> {selectedDate.toLocaleDateString()}</div>
+    <div><strong>Horário:</strong> {horarioSelecionado.horarioInicio} - {horarioSelecionado.horarioFim}</div>
+  </div>
+  <div className="mb-2"> {/* Adiciona margem superior para afastar o campo do conteúdo acima */}
+    <label><strong>Produto:</strong></label>
+    <select className="border rounded p-1 w-full" value={produto} onChange={(e) => setProduto(e.target.value)}>
+      <option value="">Selecione</option>
+      {produtos.map((produto) => (
+        <option key={produto.CodigoProduto} value={produto.CodigoProduto}>
+          {produto.DescricaoProduto}
+        </option>
+      ))}
+    </select>
+  </div>
+  <div>
+    <label><strong>Quantidade:</strong></label>
+    <input
+      type="text"
+      className="border rounded p-1 w-full"
+      placeholder="Quantidade"
+      value={quantidade}
+      onChange={(e) => setQuantidade(e.target.value)}
+    />
+  </div>
+  <div>
+    
+    <label><strong>Observação:</strong></label>
+    <textarea
+      className="border rounded p-2 w-full"
+      placeholder="Insira uma observação"
+      value={observacao}
+      onChange={(e) => setObservacao(e.target.value)}
+    ></textarea>
+  </div>
+  <div>
+    <label><strong>Upload de Arquivo:</strong></label>
+    <input type="file" className="border rounded p-2 w-full" onChange={(e) => setArquivo(e.target.files?.[0] || null)} />
+  </div>
+</div>
+
+      <button
+        onClick={handleAgendar}
+        className="bg-blue-500 text-white py-2 px-4 rounded mt-4"
+      >
+        Confirmar Agendamento
+      </button>
+    </div>
+  </div>
+</Modal>
+
+  
+  
   );
 };
 
