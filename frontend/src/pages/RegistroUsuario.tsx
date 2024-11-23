dca213eb14a67cdd72f6abc03aceca7897946561
Blackburn060 <Blacktauz30@outlook.com>
2024-11-22 21:40:41 -0300

    Implementação de confirmação de e-mail e senha na tela de cadastro de usuário.

--- frontend/src/pages/RegistroUsuario.tsx
+++ frontend/src/pages/RegistroUsuario.tsx
@@ -12,7 +12,9 @@ const RegistroUsuario: React.FC = () => {
   const [formData, setFormData] = useState({
     nomeCompleto: '',
     email: '',
+    confirmarEmail: '',
     senha: '',
+    confirmarSenha: '',
     cpf: '',
   });
   const [loading, setLoading] = useState(false);
@@ -31,8 +33,9 @@ const RegistroUsuario: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-  
+
     try {
+      // Validação do CPF
       const cleanedCPF = formData.cpf.replace(/\D/g, '');
       if (!cpfValidator.isValid(cleanedCPF)) {
         toast.error('CPF inválido. Verifique e tente novamente.');
@@ -40,21 +43,32 @@ const RegistroUsuario: React.FC = () => {
         return;
       }
 
+      // Validação de e-mail
+      if (formData.email !== formData.confirmarEmail) {
+        toast.error('Os e-mails não coincidem. Verifique e tente novamente.');
+        setLoading(false);
+        return;
+      }
+
+      // Validação de senha
+      if (formData.senha !== formData.confirmarSenha) {
+        toast.error('As senhas não coincidem. Verifique e tente novamente.');
+        setLoading(false);
+        return;
+      }
+
+      // Verificação de e-mail no servidor
       const { exists, active } = await checkEmailExistsPublic(formData.email);
-  
+
       if (exists && active) {
         toast.error('E-mail já utilizado em outra conta. Por favor, use outro e-mail.');
         setLoading(false);
         return;
       }
-  
-      if (exists && !active) {
-        localStorage.setItem('registroUsuario', JSON.stringify(formData));
-        navigate('/registro/transportadora');
-      } else {
-        localStorage.setItem('registroUsuario', JSON.stringify(formData));
-        navigate('/registro/transportadora');
-      }
+
+      // Salvar dados e redirecionar
+      localStorage.setItem('registroUsuario', JSON.stringify(formData));
+      navigate('/registro/transportadora');
     } catch (err) {
       toast.error('Erro ao verificar o e-mail. Tente novamente.');
     } finally {
@@ -69,15 +83,22 @@ const RegistroUsuario: React.FC = () => {
         <img src={imagemLateralLogin} alt="Image Login Screen" className="w-auto h-full object-contain" />
       </div>
       <div className="w-full lg:w-1/2 flex flex-col justify-center items-center bg-logisync-color-blue-400 lg:rounded-lg lg:rounded-r-lg lg:rounded-l-none">
-        <form onSubmit={handleSubmit} className="w-full max-w-sm overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-transparent scrollbar-thumb-rounded-full">
-
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-sm overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-transparent scrollbar-thumb-rounded-full"
+        >
           <div className="mb-6">
-            <h1 className="bg-logisync-color-blue-50 text-white text-2xl font-extrabold py-2 w-full rounded flex items-center justify-center">Insira seus dados</h1>
+            <h1 className="bg-logisync-color-blue-50 text-white text-2xl font-extrabold py-2 w-full rounded flex items-center justify-center">
+              Insira seus dados
+            </h1>
           </div>
+
+          {/* Nome Completo */}
           <div className="mb-4">
             <label className="flex text-white text-lg font-bold mb-1" htmlFor="nomeCompleto">
               Nome Completo
-              <FaAsterisk size={11} color='red' className='ml-1' /></label>
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
             <input
               type="text"
               id="nomeCompleto"
@@ -89,10 +110,13 @@ const RegistroUsuario: React.FC = () => {
               required
             />
           </div>
+
+          {/* CPF */}
           <div className="mb-4">
             <label className="flex text-white text-lg font-bold mb-1" htmlFor="cpf">
               CPF
-              <FaAsterisk size={11} color='red' className='ml-1' /></label>
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
             <Cleave
               id="cpf"
               name="cpf"
@@ -104,10 +128,13 @@ const RegistroUsuario: React.FC = () => {
               required
             />
           </div>
+
+          {/* E-mail */}
           <div className="mb-4">
             <label className="flex text-white text-lg font-bold mb-1" htmlFor="email">
               E-mail
-              <FaAsterisk size={11} color='red' className='ml-1' /></label>
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
             <input
               type="email"
               id="email"
@@ -119,10 +146,31 @@ const RegistroUsuario: React.FC = () => {
               required
             />
           </div>
-          <div className="mb-6">
+
+          {/* Confirmar E-mail */}
+          <div className="mb-4">
+            <label className="flex text-white text-lg font-bold mb-1" htmlFor="confirmarEmail">
+              Confirmar E-mail
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
+            <input
+              type="email"
+              id="confirmarEmail"
+              name="confirmarEmail"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Confirme o seu e-mail"
+              value={formData.confirmarEmail}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          {/* Senha */}
+          <div className="mb-4">
             <label className="flex text-white text-lg font-bold mb-1" htmlFor="senha">
               Senha
-              <FaAsterisk size={11} color='red' className='ml-1' /></label>
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
             <input
               type="password"
               id="senha"
@@ -134,6 +182,26 @@ const RegistroUsuario: React.FC = () => {
               required
             />
           </div>
+
+          {/* Confirmar Senha */}
+          <div className="mb-6">
+            <label className="flex text-white text-lg font-bold mb-1" htmlFor="confirmarSenha">
+              Confirmar Senha
+              <FaAsterisk size={11} color="red" className="ml-1" />
+            </label>
+            <input
+              type="password"
+              id="confirmarSenha"
+              name="confirmarSenha"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Confirme a sua senha"
+              value={formData.confirmarSenha}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          {/* Botão de Enviar */}
           <div className="mb-4">
             <button
               type="submit"
